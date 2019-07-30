4cf349717703cf4165caa7938d5d20e38cc5450b
wanfanfan <wanfanfan@log56.com>
2019-07-30 11:41:15 +0800

    我在master修改了demo1

--- src/main/java/com/wtyt/base/git/GitDemo1.java
+++ src/main/java/com/wtyt/base/git/GitDemo1.java
@@ -6,6 +6,7 @@ public class GitDemo1 {
 		System.out.println("这个是测试代码");
 		System.out.println("我在master修改了点东西");
 		System.out.println("我在这加了点东西");
+		System.out.println("我在master修改了demo1");
 	}
 
 }
